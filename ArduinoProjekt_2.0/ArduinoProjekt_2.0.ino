2301971ce8a39f3af50a686cad8dac0bbce56840
Frostplexx <62436912+Frostplexx@users.noreply.github.com>
2021-01-22 12:17:22 +0100

    cleaned code

--- ArduinoProjekt_2.0/ArduinoProjekt_2.0.ino
+++ ArduinoProjekt_2.0/ArduinoProjekt_2.0.ino
@@ -1,20 +1,17 @@
-
-
-#include <SFE_BMP180.h>
-#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <Adafruit_BMP085.h>
+#include <Wire.h>
 #include <DHT.h>
 #define DHTPIN 2          // Hier die Pin Nummer eintragen wo der Sensor angeschlossen ist
 #define DHTTYPE DHT22
 
 // You will need to create an SFE_BMP180 object, here called "pressure":
 
-SFE_BMP180 pressure;
+Adafruit_BMP085 pressure;
 
 #define ALTITUDE 1655.0 // Altitude of SparkFun's HQ in Boulder, CO. in meters
 DHT dht(DHTPIN, DHTTYPE);
-LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
-
+LiquidCrystal_I2C lcd(0x27,20,4);
 
 byte customChar[] = {
   B00011,
@@ -27,53 +24,23 @@ byte customChar[] = {
   B00000
 };
 
-float pressureArray[14];
-float pressureChange;
-
-
-
 void setup()
 {
-  lcd.begin(20, 4);
+
+  lcd.init();
+  lcd.backlight();
   lcd.createChar(0, customChar);
-  lcd.home();
 
-
-  Serial.begin(9600);
-  Serial.println("REBOOT");
-
-
-  if (pressure.begin())
-    Serial.println("BMP180 init success");
-  else
-  {
-  
-
-    Serial.println("BMP180 init fail\n\n");
-    while (1); // Pause forever.
-  }
   dht.begin();
 
+  Serial.begin(9600);
+  if (!pressure.begin()) {
+	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+	while (1) {}
+  }
+
 }
 
-//draw battery level in position x,y
-void batterylevel()
-{
- 
-}
-//read internal voltage
-long readVcc() {
-  long result;
-  // Read 1.1V reference against AVcc
-  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
-  delay(2); // Wait for Vref to settle
-  ADCSRA |= _BV(ADSC); // Convert
-  while (bit_is_set(ADCSRA, ADSC));
-  result = ADCL;
-  result |= ADCH << 8;
-  result = 1126400L / result; // Back-calculate AVcc in mV
-  return result;
-}
 
 void loop()
 {
@@ -81,44 +48,6 @@ void loop()
   char status;
   double T, P, p0, a;
 
-  status = pressure.startTemperature();
-  if (status != 0)
-  {
-    // Wait for the measurement to complete:
-    delay(status);
-
-    status = pressure.getTemperature(T);
-    if (status != 0)
-    {
-
-      status = pressure.startPressure(3);
-      if (status != 0)
-      {
-        // Wait for the measurement to complete:
-        delay(status);
-
-        status = pressure.getPressure(P, T);
-        if (status != 0)
-        {
-          // Print out the measurement:
-          lcd.setCursor(0, 4);
-          lcd.print("Pressure: ");
-          lcd.print(P, 2);
-          lcd.print("hPa");
-        }
-        else Serial.println("error retrieving pressure measurement\n");
-      }
-      else Serial.println("error starting pressure measurement\n");
-    }
-    else Serial.println("error retrieving temperature measurement\n");
-  }
-  else Serial.println("error starting temperature measurement\n");
-
-  float h = dht.readHumidity();
-  lcd.setCursor(0, 2);
-  lcd.print("Humidity: ");
-  lcd.print(h);
-  lcd.print("%");
   float t = dht.readTemperature();
   lcd.setCursor(0, 0);
   lcd.print("Temp: ");
@@ -126,66 +55,26 @@ void loop()
   lcd.write(byte(0));
   lcd.print("C");
 
-
-  for (int a = 13 ; a >= 0 ; --a )
-  {
-    pressureArray[a] = pressureArray[a - 1];
-  }
-  pressureArray[0] = P;
-  pressureChange = pressureArray[13] - pressureArray[0];
+  float h = dht.readHumidity();
   lcd.setCursor(0, 1);
-  lcd.print("Weather: ");
-  if (pressureArray[13] != 0 && pressureChange >= 3000)
-  {
-    lcd.print("Rain (WIP)");
-  }
-  else
-  {
-    lcd.print("Sun (WIP)");
-  }
+  lcd.print("Humidity: ");
+  lcd.print(h);
+  lcd.print("%");
 
 
- //read the voltage and convert it to volt
-  double curvolt = double( readVcc() ) / 1000;
- // loop(curvolt); 
-  // check if voltge is bigger than 4.2 volt so this is a power source
-  if(curvolt > 4.2)
-  {
-    lcd.setCursor(15, 0);  
-    lcd.print(curvolt);
-    lcd.print("V"); 
-  }
-  if(curvolt <= 4.2 && curvolt > 4.0)
-  {
+  lcd.setCursor(0, 2);
+  lcd.print("Pressure: ");
+  lcd.print((float)pressure.readPressure()/100);
+  lcd.print("hPa");
 
-  }
-  if(curvolt <= 4.0 && curvolt > 3.8)
-  {
+  lcd.setCursor(0, 3);
+  lcd.print("CO2: ");
+  lcd.print("1000");
+  lcd.print("ppm");
 
 
-  }
-  if(curvolt <= 3.8 && curvolt > 3.6)
-  {
 
-
-  }
-  if(curvolt <= 3.6 && curvolt > 3.4)
-  {
-
-  }
-  if(curvolt <= 3.4 && curvolt > 3.2)
-  {
-
-  }
-  if(curvolt <= 3.2 && curvolt > 3.0)
-  {
-
-  }
-  if(curvolt < 3.0)
-  {
-
-  }
    
-    delay(10000);  // Pause for 10 seconds.
+    delay(1000);  // Pause for 10 seconds.
 
   }
